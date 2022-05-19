cd8f41db99c1ca5766b26dc8b31a7f34714b38de
nguyen_thanh_nam <{namnguyenthanh2k1@gmail.com}>
2022-05-19 09:32:36 +0700

    phan file js class_expand_delete.js

--- scripts/class_expand.js
+++ scripts/class_expand.js
@@ -5,13 +5,14 @@ app.classExpandCtrl = function ($scope, $location) {
             (belong != null || belong != undefined)
         ) {
             var index_delete = $scope.klasses.findIndex(temp => temp.name === name);
+            var index = $scope.klasses.findIndex(temp => temp.name === belong);
 
             $scope.klasses.splice(index_delete, 1);
 
-            var index = $scope.klasses.findIndex(temp => temp.name === belong);
             var tempOrderByBefore = $scope.klasses[index].orderBy * 1;
             var tempOrderByAfter = $scope.klasses[index + 1].orderBy * 1;
             var result = (tempOrderByBefore + tempOrderByAfter) / 2;
+
             var tempKlass = {
                 name: name,
                 parentID: $scope.klasses[index].parentID,
@@ -24,43 +25,6 @@ app.classExpandCtrl = function ($scope, $location) {
             $scope.klasses.push(tempKlass);
 
             $location.path('/class');
-        }
-    }
-
-    $scope.maxLevel = function (object) {
-        var max = 0;
-
-        for (var i = 1; i < object.length; i++) {
-            if (max < object[i].level) {
-                max = object[i].level;
-            }
-        }
-        return max;
-    }
-
-    $scope.count = function (object, level, parentID) {
-        var count = 0;
-
-        for (var i = 0; i < object.length; i++) {
-            if (level < object[i].level && object[i].level <= $scope.maxLevel($scope.klasses) && object[i].parentID == parentID) {
-                count++;
-            }
-        }
-        return count;
-    };
-
-    $scope.deleteClass = function (klass) {
-        var index = $scope.klasses.indexOf(klass);
-        var tempLevel = $scope.klasses[index].level;
-        var tempParentID = $scope.klasses[index].parentID;
-
-        $scope.klasses.splice(index, 1);
-
-        for (var i = 0; i <= $scope.klasses.length; i++) {
-            if ($scope.klasses[i].level > tempLevel &&
-                $scope.klasses[i].parentID == tempParentID) {
-                return $scope.klasses.splice(i, $scope.count($scope.klasses, tempLevel, tempParentID));
-            }
         }
     }
 }
